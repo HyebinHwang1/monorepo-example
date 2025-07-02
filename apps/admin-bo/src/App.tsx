7cb35fa353c9a917cf70db0ca45a084e573af53d
HyebinHwang1 <medi.bin@mediquitous.com>
2025-07-02 15:56:21 +0900

    Fix: 사용하지 않는 gitignore 파일 삭제

--- apps/admin-bo/src/App.tsx
+++ apps/admin-bo/src/App.tsx
@@ -1,13 +1,8 @@
 import "./App.css";
 
 function App() {
-  const hi = "hi";
-  const good = "good";
-
-
   return (
     <>
-      <div>123</div>
       <div>123</div>
       <div>123</div>
       <div>123</div>
