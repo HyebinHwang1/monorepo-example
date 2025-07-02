86211a68ff4947c12144ff8f5992a6ec81be43ce
HyebinHwang1 <medi.bin@mediquitous.com>
2025-07-02 15:44:26 +0900

    Init: first commit

---
+++ apps/admin-bo/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const hi = "hi";
+  const good = "good";
+
+
+  return (
+    <>
+      <div>123</div>
+      <div>123</div>
+      <div>123</div>
+      <div>123</div>
+    </>
+  );
+}
+
+export default App;
